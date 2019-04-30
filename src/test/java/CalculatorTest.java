208f869dad26c8db2a0a9dcd7f66e08361d0a225
gigix <gigix1980@gmail.com>
2019-04-30 16:04:09 +0800

    rename a test class to make more sense

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class SmokeTest {
+public class CalculatorTest {
     @Test
     public void should_be_able_to_run_a_trivial_test_case() {
         assertThat(Calculator.add(1, 1), is(2));
