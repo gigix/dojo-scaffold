0d464aa2084d265a95f60914a52260175f68321e
Jeff Xiong <gigix@Jeff-Star-Stream-MBP.local>
2019-08-07 19:52:51 +0800

    clean house

--- src/test/java/org/codingdojo/kata/args/SchemaTest.java
+++ src/test/java/org/codingdojo/kata/args/SchemaTest.java
@@ -2,9 +2,6 @@ package org.codingdojo.kata.args;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class SchemaTest {
     @Test
     public void should_create_schema_out_of_text() {
@@ -13,14 +10,12 @@ public class SchemaTest {
         //  Schema schema = new Schema("l:boolean p:integer d:string");
         //  记住：只写最少的、能让测试通过的代码
 
-        Schema schema = new Schema("l:boolean p:integer d:string");
 
 
         // 2. Schema的size方法会告诉我们，这个Schema对象包含几个参数规格
         //  写一个断言验证size方法
         //  写最少的代码让测试通过
 
-        assertThat(schema.size(), is(3));
 
 
         // 3. 思考一下，如果要从Schema中取得某个参数（例如参数"l"）的类型（"boolean"）
@@ -34,7 +29,6 @@ public class SchemaTest {
         //  只验证正确的情况，暂时不考虑错误（例如：参数名称不存在）的情况
         //  写最少的代码让测试通过
         //  注意观察：这一步是不是迈得特别大？可以如何缩小步伐？
-        assertThat(schema.typeOf("l"), is("boolean"));
 
 
 
