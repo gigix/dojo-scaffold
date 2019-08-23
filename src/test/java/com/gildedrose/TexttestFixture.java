2f527bb4462246146fe5274322e25273931f6449
Jeff Xiong <gigix1980@gmail.com>
2019-08-23 12:31:42 +0800

    make the initial baseline more reliable

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -13,12 +13,13 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
+        int days = 3;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
