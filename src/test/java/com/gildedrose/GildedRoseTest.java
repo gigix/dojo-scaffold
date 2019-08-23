10ed6a943ff2f3464ece515431e55fefaa44ff19
Jeff Xiong <gigix1980@gmail.com>
2019-08-23 12:27:19 +0800

    go back to the beginning

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,5 @@
 package com.gildedrose;
 
-import static com.gildedrose.Item.createNormalItem;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -10,9 +9,9 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { createNormalItem("foo", 1, 5) };
+        Item[] items = new Item[] { new Item("foo", 1, 5) };
         GildedRose app = new GildedRose(items);
-        app.passOneDay();
+        app.updateQuality();
         assertEquals("foo", app.items[0].name);
         assertThat(app.items[0].quality, is(4));
         assertThat(app.items[0].sellIn, is(0));
