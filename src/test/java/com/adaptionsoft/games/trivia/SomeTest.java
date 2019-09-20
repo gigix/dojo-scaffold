42e0f1d6a22392ec08b3e94f02fbd62f46f5dbaf
Jeff Xiong <gigix1980@gmail.com>
2019-09-20 19:50:13 +0800

    ready to begin

---
+++ src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.trivia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeTest {
+
+    @Test
+    public void true_is_true() {
+        assertTrue(false);
+    }
+}
